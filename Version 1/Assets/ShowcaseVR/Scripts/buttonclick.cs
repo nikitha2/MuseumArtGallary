ea81868371dcb74c1d51ff99cdabeb9719d4b259
Nikitha Gullapalli <gullapalli.nikitha@gmail.com>
2018-09-08 13:38:09 -0400

    Add files via upload

---
+++ Version 1/Assets/ShowcaseVR/Scripts/buttonclick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonclick : MonoBehaviour
+{
+    public GameObject startUI, restartUI;
+
+    public void StartPuzzle()
+    {
+        // Disable the start UI.
+        startUI.SetActive(false);
+    }
+
+    // Reset the puzzle sequence.
+    public void ResetPuzzle()
+    {
+        // Enable the start UI.
+        startUI.SetActive(true);
+
+        // Disable the restart UI.
+        restartUI.SetActive(false);
+    }
+}
+
